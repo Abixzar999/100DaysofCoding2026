41eeabce672c6645bdee16e0c4b14c0d331b8c05
Abixzar999 <abigifari21@gmail.com>
2026-09-02 23:15:57 +0800

    Create day1.java
    days1

---
+++ day1.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // membuat data nama
+        String nama = "Abizar";
+        /*
+        * membuat data umur 
+        *menggunakan tipe data
+    int
+        */
+        int umur = 18;
+    
+        System.out.println("Nama  : " + nama);
+        System.out.println("Umur  : " + umur);
+
+    }
+}
