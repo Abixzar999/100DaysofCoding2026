4daecfd9d33e8f5f315f1dc7c5b0738c127dfe2b
Abixzar999 <abigifari21@gmail.com>
2026-09-03 22:12:55 +0800

    Create day2.java
    days2

---
+++ day2.java
@@ -0,0 +1,28 @@
+
+/**
+ * @author abizar
+ * days2
+ */
+public class days2 {
+
+    public static void main(String[] args) {
+
+        String nama = "Abizar";
+        int umur = 18;
+        double tinggi = 160;
+
+        //Print & println
+        System.out.print("==BIODATA==");
+        System.out.println("");
+
+        //printf
+        System.out.printf("perkenalkan nama saya %s umur saya %d tahun dan tinggi saya %.1f %n", nama, umur, tinggi);
+
+        //println
+        System.out.println("");
+        System.out.println("Nama : " + nama);
+        System.out.println("Umur : " + umur);
+        System.out.println("Tinggi : " + tinggi);
+
+    }
+}
