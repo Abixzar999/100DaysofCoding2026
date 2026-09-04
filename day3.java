971a793310b80efff51c966bd7bc80c07283f7ef
Abixzar999 <abigifari21@gmail.com>
2026-09-04 14:48:10 +0800

    Create day3.java
    days3coding

---
+++ day3.java
@@ -0,0 +1,16 @@
+
+public class days3w {
+
+    public static void main(String[] args) {
+
+        System.out.println("Perkenalkan nama Saya Abizar Al Gifari\f"); // \f untuk membuat paragraf baru
+        System.out.println("Abizar\tAl\tGifari"); // \t untuk tab atau spasi panjang
+        System.out.println("\"Abizar Al Gifari\""); // \" untuk memberi tanda kutip 2 "
+        System.out.println("Abizar\n Al \nGifari"); // \n untuk membuat baris bawah
+        System.out.println("Abizar\b Al Gifari\b "); // \b untuk menghapus 1 karakter
+        System.out.println("\'Abizar Al Gifari\'"); // \' untuk memberi tanda kutip 1 '
+        System.out.println("Abizar\rAl \rGifari"); // \ untuk menimpa atau menggantikan teks
+        System.out.println("Abizar\\Al\\Gifari"); // \\ untuk memberikan tanda slash
+
+    }
+}
