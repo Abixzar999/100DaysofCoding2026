28951ee86937062f2b25e487c5a97d6c7aa11438
Abixzar999 <abigifari21@gmail.com>
2026-09-05 12:30:14 +0800

    Create day4.java
    days4

---
+++ day4.java
@@ -0,0 +1,29 @@
+
+public class days4 {
+
+    /*days4coding
+    * Frofile game
+    * @author Abizar
+     */
+    public static void main(String[] args) {
+
+        // Byte untuk menyimpan bilangan bulat dengan nilai yang kecil
+        byte level = 10;
+        System.out.println("Level : " + level);
+
+        // Short untuk menyimpan bilangan bulat dengan nilai sedang atau lebih besar dari Byte
+        short jumlahkoin = 2400;
+        System.out.println("Jumlah Koin : " + jumlahkoin);
+
+        // Int untuk menyimpan bilangan bulat cukup besar
+        // Tipe data ini yang sering digunakan untuk menyimpan bilangan bulat
+        int hargaitem = 20000;
+        System.out.println("Harga Item : " + hargaitem);
+
+        long jumlahpemainonline = 1010000003000L;
+        System.out.println("Jumlah Pemain Online : " + jumlahpemainonline);
+        // Long untuk menyimpan bilagan bulat yang paling besar
+        // Tipe data long biasanya penulisannya diakhiri dengan huruf L
+        
+    }
+}
