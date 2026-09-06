73911c494364e11ec444d94bc0ca553862617981
Abixzar999 <abigifari21@gmail.com>
2026-09-06 13:38:53 +0000

    Create day5.java
    days5ofcoding

---
+++ day5.java
@@ -0,0 +1,17 @@
+
+public class days5 {
+
+    public static void main(String[] args) throws Exception {
+
+        //Tipe data float digunakan untuk menyimpan angka pecahan/desimal
+        float beratayam = 12.1f;
+
+        //Tipe data double untuk menyimpan anga pecahan/ desimal degan ketelitian lebih tinggi
+        double hargaayam = 250000.55;
+
+        // menampilkan variabel
+        System.out.println("Berat Ayam : " + beratayam + " kg ");
+        System.out.println("Harga Ayam : " + hargaayam + " rupiah ");
+
+    }
+}
