3561ada3b730e2e55745e37602e89cb4dca7226c
Abixzar999 <abigifari21@gmail.com>
2026-09-07 03:45:30 +0000

    Create day6.java
    days6ofcoding

---
+++ day6.java
@@ -0,0 +1,23 @@
+
+public class days6 {
+
+    /*
+    * @author Abizar
+    * Days6
+     */
+    public static void main(String[] args) {
+
+        //tipe data char adalah tipe data yang menggunakan 1 char
+        char rank = 'A';
+
+        //tipe data menyimpan nilai benar atau salah
+        boolean menang = true;
+        boolean kalah = false;
+
+        //menampilkan variable
+        System.out.println("Rank : " + rank);
+        System.out.println("Menang : " + menang);
+        System.out.println("Kalah : " + kalah);
+    }
+
+}
