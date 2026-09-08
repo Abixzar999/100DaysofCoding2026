109554324bcffc09a670e5df1ceaabd6ca46dab0
Abixzar999 <abigifari21@gmail.com>
2026-09-08 05:57:46 +0000

    Create day7.java
    Pengenalan Tipe Data String untuk teks.

---
+++ day7.java
@@ -0,0 +1,19 @@
+
+public class Days7 {
+
+    /*
+* Days7
+* @author Abizar
+     */
+    public static void main(String[] args) {
+
+        //Tipe data String yaitu digunakan untuk menyimpan teks
+        String nama = "Abizar";
+        String nim = "D0226504";
+
+        //Menampilkan variable  
+        System.out.println("Nama : " + nama);
+        System.out.println("NIM : " + nim);
+    }
+
+}
