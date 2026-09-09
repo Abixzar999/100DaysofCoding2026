ee2a3bb457af16e7fd3e9da334e491fb4aaf5e27
Abixzar999 <abigifari21@gmail.com>
2026-09-09 15:23:10 +0800

    Create day8.java
    Deklarasi, Inisialisasi dan Update Variabel.

---
+++ day8.java
@@ -0,0 +1,31 @@
+
+public class days8 {
+
+    /*
+        * @author Abizar
+        * Days8
+     */
+    public static void main(String[] args) {
+
+        /*
+        * @author Abizar
+        * Days8
+        */
+        
+        //Deklarasi 
+        //Yaitu membuat variable, tapi belum memberikan isi
+        int ram;
+
+        //Inisialisasi
+        //Yaitu memberi nilai awal pada variable
+        ram = 8;
+        System.out.println("RAM : " + ram + " GB ");
+
+        //Update
+        //Yaitu menguba variable yang sudah ada
+        ram = 16;
+        System.out.println("RAM Ditambahkan : " + ram + " GB ");
+
+    }
+}
+ 
