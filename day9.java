639ea38e50ee227957ede7b7c95839144028e3f2
Abixzar999 <abigifari21@gmail.com>
2026-09-10 20:52:31 +0800

    Create day9.java
    Constanta (Final Variabel).

---
+++ day9.java
@@ -0,0 +1,18 @@
+
+public class days9 {
+
+     /*
+     * @author Abizar
+     * Days9
+     */
+    public static void main(String[] args) {
+
+        final int HARGA_BAJU = 25000;
+        //final digunakan untuk membuat variable yang nilainya tidak bisa diubah lagi setelah diberi nilai
+
+        System.out.println("Harga Baju : Rp" + HARGA_BAJU);
+        //menampilkan variable
+
+    }
+
+}
