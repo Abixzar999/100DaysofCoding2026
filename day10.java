77ffaadd21182a8981c34da94956cef6be33371b
Abixzar999 <abigifari21@gmail.com>
2026-09-11 20:44:52 +0800

    Create day10.java
    Pemahaman Batas Minimal dan Maksimal dari tipe data

---
+++ day10.java
@@ -0,0 +1,28 @@
+
+public class days10 {
+
+    public static void main(String[] args) {
+
+        System.out.println("byte : " + Byte.MIN_VALUE + " s/d " + Byte.MAX_VALUE);
+        //Menampilkan batas minimal dan maksimal tipe data byte
+
+        System.out.println("short : " + Short.MIN_VALUE + " s/d " + Short.MAX_VALUE);
+        // Menampilkan batas minimal dan maksimal tipe data short
+
+        System.out.println("int : " + Integer.MIN_VALUE + " s/d " + Integer.MAX_VALUE);
+        // Menampilkan batas minimal dan maksimal tipe data int
+
+        System.out.println("long : " + Long.MIN_VALUE + " s/d " + Long.MAX_VALUE);
+        // Menampilkan batas minimal dan maksimal tipe data long
+
+        System.out.println("float : " + Float.MIN_VALUE + " s/d " + Float.MAX_VALUE);
+        // Menampilkan batas nilai tipe data float
+
+        System.out.println("double : " + Double.MIN_VALUE + " s/d " + Double.MAX_VALUE);
+        // Menampilkan batas nilai tipe data double
+
+        System.out.println("char : " + (int) Character.MIN_VALUE + " s/d " + (int) Character.MAX_VALUE);
+        // Menampilkan batas minimal dan maksimal char dalam bentuk angka
+    }
+
+}
