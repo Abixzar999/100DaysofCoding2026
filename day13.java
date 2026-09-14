b1f2de058e374fe811ca46ebbb25078f79b8f7b2
Abixzar999 <abigifari21@gmail.com>
2026-09-14 14:26:14 +0000

    Create day13.java
    evaluasi

---
+++ day13.java
@@ -0,0 +1,80 @@
+
+import java.util.Scanner;
+
+public class evaluasi_1 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        // Soal nomor 1
+        
+        // byte mybyte;
+        // int myint;
+        // float myfloat;
+        // double mydouble;
+        // System.out.println("=====SEBELUM UPDATE=====");
+        // mybyte = 3;
+        // myint = 10;
+        // myfloat = 5.5f;
+        // mydouble = 10.5;
+        // System.out.println("Byte\t: " + mybyte);
+        // System.out.println("Int\t: " + myint);
+        // System.out.println("Float\t: " + myfloat);
+        // System.out.println("Double\t: " + mydouble);
+        // System.out.println("\n===== SESUDAH UPDATE =====");
+        // mybyte = 4;
+        // myint = 20;
+        // myfloat = 6.5f;
+        // mydouble = 20.5;
+        // System.out.println("Byte\t: " + mybyte);
+        // System.out.println("Int\t: " + myint);
+        // System.out.println("Float\t: " + myfloat);
+        // System.out.println("Double\t: " + mydouble);
+
+        // Soal nomor 2
+
+        // String nama = "Iqra";
+        // int umur = 19;
+        // System.out.printf("%nBIODATA MAHASISWA%n");
+        // System.out.printf("Nama\t\t: %s%n", nama);
+        // System.out.printf("Umur\t\t: %d%n", umur);
+        // System.out.println("Tahun Masuk\t: 2025 ");
+        // System.out.println("Jumlah SKS\t: 24 ");
+        // System.out.println("NIM\t\t: 225015 ");
+        // System.out.println("Tinggi Badan\t: 167.5 cm ");
+        // System.out.println("IPK\t\t: 3,94 ");
+        // System.out.println("Kelas\t\t: B ");
+        // System.out.print("Status Aktif\t: true");
+        // System.out.println("\n====================================");
+
+        // Soal nomor 3
+        
+        // System.out.print("Nama : ");
+        // String nama = input.nextLine();
+        // System.out.print("Umur : ");
+        // int umur = input.nextInt();
+        // System.out.print("Tanggal lahir : ");
+        // short tanggallahir = input.nextShort();
+        // System.out.print("Bulan lahir : ");
+        // byte bulanlahir = input.nextByte();
+        // System.out.print("Tahun lahir : ");
+        // long tahunlahir = input.nextLong();
+        // System.out.print("Berat Badan : ");
+        // float beratbadan = input.nextFloat();
+        // System.out.print("Tinggi badan : ");
+        // double tinggibadan = input.nextDouble();
+        // System.out.print("Kelas : ");
+        // char kelas = input.next().charAt(0);
+        // System.out.print("Status Mahasiswa : ");
+        // boolean status = input.nextBoolean();
+        // System.out.println("=======BIODATA=======");
+        // System.out.println("Nama\t\t\t: " + nama);
+        // System.out.println("Umur\t\t\t: " + umur);
+        // System.out.println("Tanggal lahir\t\t: " + tanggallahir + " " + bulanlahir + " " + tahunlahir);
+        // System.out.println("Berat badan\t\t: " + beratbadan);
+        // System.out.println("Tinggi badan\t\t: " + tinggibadan);
+        // System.out.println("Kelas\t\t\t: " + kelas);
+        // System.out.println("Status Mahasiswa\t:" + status);
+    }
+}
