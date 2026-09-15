b9ca53071f902915e235f0a035ecabf5dbc7bb05
Abixzar999 <abigifari21@gmail.com>
2026-09-15 13:01:52 +0000

    Create day14.java
    Operator Aritmatika Penjumlahan (+) dan Pengurangan (-)

---
+++ day14.java
@@ -0,0 +1,26 @@
+public class App {
+    public static void main(String[] args) {
+
+        // Menentukan dua angka untuk penjumlahan
+        int angka1 = 20;
+        int angka2 = 10;
+
+        // Melakukan penjumlahan
+        int hasil = angka1 + angka2;
+
+        // Menampilkan hasil penjumlahan
+        System.out.println("Hasil :" + hasil);
+
+        // Menentukan dua angka untuk pengurangan
+        int angka3 = 20;
+        int angka4 = 10;
+
+        // Melakukan pengurangan
+        int hasil2 = angka3 - angka4;
+
+        // Menampilkan hasil pengurangan
+        System.out.println("Hasil :" + hasil2);
+
+        //Operator Aritmatika Penjumlahan (+) dan Pengurangan (-)
+    }
+}
