7d3b58ea22e3d24f45af6903e64b702e87c14e0c
Abixzar999 <abigifari21@gmail.com>
2026-09-16 12:25:51 +0000

    Create day15.java
    perkalian dan pembagian

---
+++ day15.java
@@ -0,0 +1,21 @@
+public class App {
+    public static void main(String[] args) {  
+
+    // Menentukan dua angka untuk operasi perkalian dan pembagian
+    int a = 25;
+    int b = 5;
+
+    // Melakukan operasi perkalian
+    int hasil = a * b;
+
+    // Menampilkan hasil perkalian
+    System.out.println("Hasil perkalian : " + hasil);
+    
+    // Menampilkan hasil pembagian
+    int hasil2 = a / b;
+    System.out.println("Hasil pembagian : " + hasil2);
+
+    // Operator aritmatika perkalian dan pembagian
+
+    }
+}
