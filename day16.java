3365698b936c80fe656b9b890e7aef8098632e87
Abixzar999 <abigifari21@gmail.com>
2026-09-17 05:47:32 +0000

    Create day16.java
    Operator aritmatika modulus atau sisa bagi (%)

---
+++ day16.java
@@ -0,0 +1,17 @@
+public class days16 {
+    public static void main(String[] args) {
+        
+        // Menentukan dua angka untuk operasi modulus
+        int a = 25;
+        int b = 3;
+
+        // Melakukan operasi modulus
+        int hasil = a % b;
+
+        // Menampilkan hasil modulus
+        System.out.println("hasil modulus dari " + a + " % " + b + " adalah " + hasil);
+        
+        // Operator aritmatika modulus atau sisa bagi (%)
+     
+    }
+}
