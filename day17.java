6785477c8fcb3f4459dd73a7dd72a7781e0d91dc
Abixzar999 <abigifari21@gmail.com>
2026-09-18 13:16:00 +0000

    Create day17.java
    operator penugasan

---
+++ day17.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class days17 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Meminta pengguna untuk memasukkan angka
+        System.out.print("Masukkan angka: ");
+        int a = input.nextInt();
+        
+        // Menambahkan 5 ke angka yang dimasukkan
+        a += 5;
+        System.out.println("Hasil penambahan 5 : " + a);
+
+        // Mengurangkan 3 dari angka yang dimasukkan
+        a -= 3;
+        System.out.println("Hasil pengurangan 3 : " + a);
+
+        // Mengalikan angka yang dimasukkan dengan 2
+        a *= 2;
+        System.out.println("Hasil perkalian 2 : " + a);
+
+        // Membagi angka yang dimasukkan dengan 4
+        a /= 4;
+        System.out.println("Hasil pembagian 4 : " + a);
+
+        // Menghitung sisa bagi dari angka yang dimasukkan dengan 3
+        a %= 3;
+        System.out.println("Hasil modulus 3 : " + a);
+
+        // Operator penugasan (=, -=, dll)
+
+        
+    }
+}
