4af0f6ad353620342cb232711c801680ea1ba6ec
Abixzar999 <abigifari21@gmail.com>
2026-09-19 12:54:59 +0000

    Create day18.java
    Konversi Otomatis Dari Tipe Data Kecil ke Besar

---
+++ day18.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class days18 {
+    public static void main(String[] args) {
+
+    Scanner input = new Scanner(System.in);
+
+        byte angka = input.nextByte();
+
+        short hasil = angka;
+        System.out.println("Hasil konversi dari byte ke short: " + hasil);
+
+        int hasil2 = angka;
+        System.out.println("Hasil konversi dari byte ke int: " + hasil2);
+        
+        long hasil3 = angka;
+        System.out.println("Hasil konversi dari byte ke long: " + hasil3);
+        
+
+        double hasil4 = angka;
+        System.out.println("Hasil konversi dari byte ke double: " + hasil4);
+
+        float hasil5 = angka;
+        System.out.println("Hasil konversi dari byte ke float: " + hasil5);
+
+        // Konversi Otomatis Dari Tipe Data Kecil ke Besar
+
+    }
+    
+}
